56ada12085ae544088989c60adaca2e1070b24e7
P. Langendoen <p.langendoen@xsarus.nl>
2025-12-29 09:21:28 +0100

    Ticket 205725 | Client + Oauth changes

---
+++ BynderExtension/Api/BynderOauth2Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webdam.Sdk.Service;
+using Webdam.Sdk.Settings;
+
+namespace Bynder.Api
+{
+    public class BynderOauth2Client
+    {
+        private readonly string _customerBynderUrl;
+
+        public BynderOauth2Client(BynderClientSettings settings)
+        {
+            _customerBynderUrl = settings.CustomerBynderUrl;
+
+            
+
+
+            // InitializeManager(settings.ConsumerKey, settings.ConsumerSecret, settings.Token, settings.TokenSecret);
+        }
+    }
+}
