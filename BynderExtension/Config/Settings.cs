db35c97259127f02c24dec90987e3562b5ee7aac
P. Langendoen <p.langendoen@xsarus.nl>
2026-01-15 11:21:06 +0100

    Ticket #206103 | Applied changes of old feature branch

--- BynderExtension/Config/Settings.cs
+++ BynderExtension/Config/Settings.cs
@@ -13,10 +13,11 @@
         public const string CronExpression = "CRON_EXPRESSION";
         public const string CvlMetapropertyMapping = "CVL_METAPROPERTY_MAPPING";
         public const string DeleteResourceOnDeleteEvent = "DELETE_RESOURCE_ON_DELETE_EVENT";
+        /** to be deprecated */
         public const string DownloadMediaType = "DOWNLOAD_MEDIA_TYPE";
+        public const string FilenameExtensionMediaTypeMapping = "FILENAME_EXTENSION_MEDIA_TYPE_MAPPING";
         public const string ExportConditions = "EXPORT_CONDITIONS";
         public const string FieldValuesToSetOnArchiveEvent = "FIELD_VALUES_TO_SET_ON_ARCHIVE_EVENT";
-        public const string FilenameExtensionMediaTypeMapping = "FILENAME_EXTENSION_MEDIA_TYPE_MAPPING";
         public const string ImportConditions = "IMPORT_CONDITIONS";
         public const string InitialAssetLoadUrlQuery = "INITIAL_ASSET_LOAD_URL_QUERY";
         public const string InRiverEntityUrl = "INRIVER_RESOURCE_URL";
