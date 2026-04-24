119b0e779ad9c72f2e008bb5dc2604cc680cc397
P. Langendoen <p.langendoen@xsarus.nl>
2026-04-24 08:12:55 +0200

    Version info

--- BynderTest/Properties/AssemblyInfo.cs
+++ BynderTest/Properties/AssemblyInfo.cs
@@ -2,16 +2,16 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("BynderTest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("XSARUS.inRiver shared library Bynder Extension")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BynderTest")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f450ac02-9e95-4ebf-bdee-2ddb0a9fe1fd")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
