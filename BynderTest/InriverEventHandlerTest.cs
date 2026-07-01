82534f943fe7e30ba22d6f87261586cade7a1758
LAN\C.van.Dam <c.van.dam@xsarus.nl>
2026-07-01 16:01:41 +0200

    211399. Better logging. debug code

---
+++ BynderTest/InriverEventHandlerTest.cs
@@ -0,0 +1,27 @@
+﻿using Bynder.Extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BynderTest
+{
+    [TestClass, Ignore("Only use for debugging")]
+    public class InriverEventHandlerTest : TestBase
+    {
+
+        [TestMethod]
+        public void Debug()
+        {
+            var extension = new InriverEventHandler()
+            {
+                Context = InRiverContext
+            };
+
+            extension.Execute(true);
+        }
+
+    }
+}
