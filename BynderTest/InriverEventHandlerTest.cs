6f4080ee189f828fea0379774938f25c3b502503
LAN\C.van.Dam <c.van.dam@xsarus.nl>
2026-07-17 15:43:37 +0200

    211399. Fix type

--- BynderTest/InriverEventHandlerTest.cs
+++ BynderTest/InriverEventHandlerTest.cs
@@ -1,5 +1,10 @@
 ﻿using Bynder.Extension;
+using Bynder.Models;
+using Bynder.Names;
+using Bynder.Utils.Helpers;
+using inRiver.Remoting.Objects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +13,32 @@ using System.Threading.Tasks;
 
 namespace BynderTest
 {
-    [TestClass, Ignore("Only use for debugging")]
+    [Ignore("Only use for debugging")]
+    [TestClass]
     public class InriverEventHandlerTest : TestBase
     {
+        private void AddConnectorState(InriverEvent inriverEvent)
+        {
+            var connectorStateName = SettingHelper.GetConnectorStateName(InRiverContext.Settings, InRiverContext.Logger, ConnectorStateIds.BynderInriverEvents);
+
+            InRiverContext.ExtensionManager.UtilityService.AddConnectorState(new ConnectorState
+            {
+                ConnectorId = connectorStateName,
+                Data = JsonConvert.SerializeObject(inriverEvent)
+            });
+        }
 
         [TestMethod]
         public void Debug()
         {
+            AddConnectorState(new InriverEvent
+            {
+                EntityId = 426398,
+                IsLink = false,
+                IsResource = true,
+                Attempt = 0
+            });
+
             var extension = new InriverEventHandler()
             {
                 Context = InRiverContext
