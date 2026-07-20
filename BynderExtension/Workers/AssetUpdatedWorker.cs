d3e3ba9fe026d72c61560446c38e609d4aaca453
LAN\C.van.Dam <c.van.dam@xsarus.nl>
2026-07-20 12:16:15 +0200

    212778. cleaner code

--- BynderExtension/Workers/AssetUpdatedWorker.cs
+++ BynderExtension/Workers/AssetUpdatedWorker.cs
@@ -290,12 +290,9 @@ namespace Bynder.Workers
                 return true;
             }
 
-            foreach (var condition in conditions)
+            if (conditions.Any(x => !GetConditionResult(asset, x)))
             {
-                if (!GetConditionResult(asset, condition))
-                {
-                    return false;
-                }
+                return false;
             }
 
             return true;
